bfb4acb88dc6c3940805ba2fe0e572c19766e676
Thomas Ghignon <thomas@everide.app>
2023-03-16 10:59:11 +0100

    🚧 [WIP] front

--- app/src/main/java/com/gmail/eamosse/imdb/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/gmail/eamosse/imdb/ui/profile/ProfileFragment.kt
@@ -1,28 +1,29 @@
-package com.gmail.eamosse.imdb.ui.favorites
+package com.gmail.eamosse.imdb.ui.profile
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.gmail.eamosse.imdb.R
 
-class FavoritesFragment : Fragment() {
+class ProfileFragment : Fragment() {
 
-    private lateinit var favoritesViewModel: FavoritesViewModel
+    private lateinit var profileViewModel: ProfileViewModel
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        favoritesViewModel =
-                ViewModelProviders.of(this).get(FavoritesViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_favorites, container, false)
+        profileViewModel =
+                ViewModelProviders.of(this).get(ProfileViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_profile, container, false)
 //        val textView: TextView = root.findViewById(R.id.text_dashboard)
-        favoritesViewModel.text.observe(viewLifecycleOwner, Observer {
+        profileViewModel.text.observe(viewLifecycleOwner, Observer {
 //            textView.text = it
         })
         return root
