c3bd83553ba10694315a265808de1c42b93a5d18
Thomas Ghignon <thomas@everide.app>
2023-03-17 10:32:46 +0100

    🚧 [WIP] add header blur / genders list

--- app/src/main/java/com/gmail/eamosse/imdb/ui/home/HomeFragment.kt
+++ app/src/main/java/com/gmail/eamosse/imdb/ui/home/HomeFragment.kt
@@ -1,15 +1,24 @@
 package com.gmail.eamosse.imdb.ui.home
 
+import android.graphics.Bitmap
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
-import androidx.navigation.fragment.NavHostFragment
+import androidx.renderscript.Allocation
+import androidx.renderscript.Element
+import androidx.renderscript.RenderScript
+import androidx.renderscript.ScriptIntrinsicBlur
+import com.gmail.eamosse.imdb.R
 import com.gmail.eamosse.imdb.databinding.FragmentHomeBinding
 import dagger.hilt.android.AndroidEntryPoint
+import android.content.Context
+import android.graphics.BitmapFactory
+import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
 
 @AndroidEntryPoint
 class HomeFragment : Fragment() {
@@ -17,6 +26,7 @@ class HomeFragment : Fragment() {
     private val homeViewModel: HomeViewModel by viewModels()
 
     private lateinit var binding: FragmentHomeBinding
+    private var bg_image: ImageView? = null
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -28,6 +38,22 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        //Get bg_image from view and add a blur effect
+        bg_image = view.findViewById(R.id.header_blur_background)
+        //val imageView = view.findViewById<ImageView>(R.id.imageView6)
+
+
+        val blurredBitmap = blurBitmap(requireContext(), R.mipmap.ic_avatar_foreground, 10)
+
+        bg_image!!.setImageBitmap(blurredBitmap)
+
+//        Picasso.get()
+//            .load(R.mipmap.ic_avatar_foreground)
+//            .resize(1500, 1500) //set a larger size for the image
+//            .centerCrop()
+//            .transform(BlurTransformation(requireContext(),50))
+//            .into(bg_image)
 
         with(homeViewModel) {
             token.observe(viewLifecycleOwner, Observer {
@@ -43,5 +69,25 @@ class HomeFragment : Fragment() {
                 //afficher l'erreur
             })
         }
+    }
+
+    fun blurBitmap(context: Context, drawableId: Int, iterations: Int): Bitmap {
+        val bitmap = BitmapFactory.decodeResource(context.resources, drawableId)
+        val inputBitmap = Bitmap.createScaledBitmap(bitmap, bitmap.width / 2, bitmap.height / 2, false)
+
+        val rs = RenderScript.create(context)
+        val blurScript = ScriptIntrinsicBlur.create(rs, Element.U8_4(rs))
+        val input = Allocation.createFromBitmap(rs, inputBitmap)
+        val output = Allocation.createTyped(rs, input.type)
+
+        var blurredBitmap = inputBitmap
+        repeat(iterations) {
+            blurScript.setRadius(10f)
+            blurScript.setInput(Allocation.createFromBitmap(rs, blurredBitmap))
+            blurScript.forEach(output)
+            output.copyTo(blurredBitmap)
+        }
+
+        return blurredBitmap
     }
 }
